687e53da8c7031aae189eba120ed4e858cfa9cf7
Berkay Torun <berkayk3211@gmail.com>
2022-11-02 17:57:55 +0100

    Custom Buttons to manage expense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Button } from '../components/Button';
+import { IconButton } from '../components/IconButton';
+import { GlobalStyles } from '../utils/styles';
 
 export const ManageExpense = ({ route, navigation }) => {
     const expenseId = route.params?.expenseId;
@@ -11,5 +14,51 @@ export const ManageExpense = ({ route, navigation }) => {
         });
     }, [navigation, isEditing]);
 
-    return <Text>Manage Expense</Text>;
+    return (
+        <View style={styles.container}>
+            <View style={styles.buttonContainer}>
+                <Button
+                    style={styles.button}
+                    mode='flat'
+                >
+                    Cancel
+                </Button>
+                <Button style={styles.button}>{isEditing ? 'Update' : 'Add'}</Button>
+            </View>
+            {isEditing && (
+                <View style={styles.deleteContainer}>
+                    <IconButton
+                        icon='trash'
+                        color={GlobalStyles.colors.error500}
+                        size={24}
+                        onPress={() => {}}
+                    />
+                </View>
+            )}
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: GlobalStyles.colors.primary800,
+        padding: 12,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        marginHorizontal: 4,
+        minWidth: 100,
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 3,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center',
+    },
+});
