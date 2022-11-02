2222b9d05cfd38f9a5d103592fead415207c3d59
Berkay Torun <berkayk3211@gmail.com>
2022-11-02 15:17:15 +0100

    Navigation

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { AllExpenses } from './screens/AllExpenses';
+import { ManageExpense } from './screens/ManageExpense';
+import { RecentExpenses } from './screens/RecentExpenses';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
+    return (
+        <BottomTabs.Navigator>
+            <BottomTabs.Screen
+                name='RecentExpenses'
+                component={RecentExpenses}
+            />
+
+            <BottomTabs.Screen
+                name='AllExpenses'
+                component={AllExpenses}
+            />
+        </BottomTabs.Navigator>
+    );
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <>
+            <StatusBar style='auto' />
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name='ExpensesOverview'
+                        component={ExpensesOverview}
+                    />
+                    <Stack.Screen
+                        name='ManageExpense'
+                        component={ManageExpense}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
